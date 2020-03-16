33be67cc2f33b99a5b7e809f029dd1d3d223bdfb
Supreeth Gowda <59509243+SupreethGowdas@users.noreply.github.com>
2020-03-16 11:48:56 +0530

    Add files via upload

--- Algorithms/src/com/java/algorithms/NumberSorting.java
+++ Algorithms/src/com/java/algorithms/NumberSorting.java
@@ -24,18 +24,18 @@ public class NumberSorting {
 		}
 		
 		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array.length; j++) {
-				if(array[i] < array[j]) {
-					int temp = array[i];
-					array[i] = array[j];
-					array[j] = temp;
+			for (int j = 0; j < array.length-1; j++) {
+				if(array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
 				}
 			}
 		}
 		
 		System.out.print("Sorted Number -> ");
-		for (int j = 0; j < array.length; j++) {
-			System.out.print(array[j]);
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
 		}
 	}
 	
